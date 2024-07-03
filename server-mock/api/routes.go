23e8d119c3ed431fd025a722965521a0e812cde7
huy <huy@nodeset.io>
2024-07-03 11:50:02 -0700

    register v2 routes

--- server-mock/api/routes.go
+++ server-mock/api/routes.go
@@ -2,13 +2,15 @@ package api
 
 const (
 	// API routes
-	DevPath             string = "dev"
-	DepositDataMetaPath string = "deposit-data/meta"
-	DepositDataPath     string = "deposit-data"
-	ValidatorsPath      string = "validators"
-	NoncePath           string = "nonce"
-	LoginPath           string = "login"
-	RegisterPath        string = "node-address"
+	DevPath               string = "dev"
+	DepositDataMetaPath   string = "deposit-data/meta"
+	DepositDataPath       string = "deposit-data"
+	ValidatorsPath        string = "validators"
+	NoncePath             string = "nonce"
+	LoginPath             string = "login"
+	RegisterPath          string = "node-address"
+	V2StakewiseModulePath string = "v2/modules/stakewise"
+	V2CorePath            string = "v2/core"
 
 	// Admin routes
 	AdminSnapshotPath      string = "snapshot"
