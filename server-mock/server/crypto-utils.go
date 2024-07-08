6d47e675e8464b13cb2ee722bfe8ef20777bc9b2
huy <huy@nodeset.io>
2024-07-08 06:51:37 -0700

    Move createSignature into its own crypto-utils file

---
+++ server-mock/server/crypto-utils.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// https://github.com/nodeset-org/nodeset-svc-mock/blob/4f7aeb08967ec1234ba510db24ec26977851a6ee/auth/auth.go#L117
+func createSignature(message []byte, privateKey *ecdsa.PrivateKey) ([]byte, error) {
+	messageHash := accounts.TextHash(message)
+	signature, err := crypto.Sign(messageHash, privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("error signing message: %w", err)
+	}
+
+	// Fix the ECDSA 'v' (see https://medium.com/mycrypto/the-magic-of-digital-signatures-on-ethereum-98fe184dc9c7#:~:text=The%20version%20number,2%E2%80%9D%20was%20introduced)
+	signature[crypto.RecoveryIDOffset] += 27
+	return signature, nil
+}
--- server-mock/server/server.go
+++ server-mock/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -13,8 +12,6 @@ import (
 	"net/url"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gorilla/mux"
 	apiv1 "github.com/nodeset-org/nodeset-client-go/api-v1"
 	apiv2 "github.com/nodeset-org/nodeset-client-go/api-v2"
@@ -264,17 +261,4 @@ func (s *NodeSetMockServer) getNodeForSession(w http.ResponseWriter, session *db
 		return nil
 	}
 	return node
-}
-
-// https://github.com/nodeset-org/nodeset-svc-mock/blob/4f7aeb08967ec1234ba510db24ec26977851a6ee/auth/auth.go#L117
-func createSignature(message []byte, privateKey *ecdsa.PrivateKey) ([]byte, error) {
-	messageHash := accounts.TextHash(message)
-	signature, err := crypto.Sign(messageHash, privateKey)
-	if err != nil {
-		return nil, fmt.Errorf("error signing message: %w", err)
-	}
-
-	// Fix the ECDSA 'v' (see https://medium.com/mycrypto/the-magic-of-digital-signatures-on-ethereum-98fe184dc9c7#:~:text=The%20version%20number,2%E2%80%9D%20was%20introduced)
-	signature[crypto.RecoveryIDOffset] += 27
-	return signature, nil
 }
