6d3429a700e3f7a4dcb5242ac02354c7d54d3b5a
huy <huy@nodeset.io>
2025-03-26 10:49:05 -0700

    new file for vaults

--- api-v3/stakewise/validators-meta.go
+++ api-v3/stakewise/validators-meta.go
@@ -18,49 +18,6 @@ const (
 	MetaPath       string = "meta"
 )
 
-type VaultsData struct {
-	Vaults []ethcommon.Address `json:"vaults"`
-}
-
-// Gets the list of vaults available on the server for the provided deployment
-func (c *V3StakeWiseClient) Vaults(ctx context.Context, logger *slog.Logger, deployment string) (VaultsData, error) {
-	// Submit the request
-	pathString := path.Join(StakeWisePrefix, deployment, VaultsPath)
-	code, response, err := common.SubmitRequest[VaultsData](c.commonClient, ctx, logger, true, http.MethodGet, nil, nil, pathString)
-	if err != nil {
-		return VaultsData{}, fmt.Errorf("error submitting vaults request: %w", err)
-	}
-
-	// Handle response based on return code
-	switch code {
-	case http.StatusOK:
-		// Success
-		return response.Data, nil
-
-	case http.StatusBadRequest:
-		switch response.Error {
-		case common.InvalidDeploymentKey:
-			// Invalid deployment
-			return VaultsData{}, common.ErrInvalidDeployment
-		}
-
-	case http.StatusUnauthorized:
-		switch response.Error {
-		case common.InvalidSessionKey:
-			// Invalid or expired session
-			return VaultsData{}, common.ErrInvalidSession
-		}
-
-	case http.StatusForbidden:
-		switch response.Error {
-		case common.InvalidPermissionsKey:
-			// The user doesn't have permission to do this
-			return VaultsData{}, common.ErrInvalidPermissions
-		}
-	}
-	return VaultsData{}, fmt.Errorf("nodeset server responded to vaults request with code %d: [%s]", code, response.Message)
-}
-
 // Returns information about the requesting user's node account with respect to the number of validators the user has deployed and can deploy on this vault.
 func (c *V3StakeWiseClient) ValidatorMeta_Get(ctx context.Context, logger *slog.Logger, deployment string, vault ethcommon.Address) (stakewise.ValidatorsMetaData, error) {
 	pathString := path.Join(StakeWisePrefix, deployment, vault.Hex(), ValidatorsPath, MetaPath)
